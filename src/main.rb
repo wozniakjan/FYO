8bbb8f3563f39fdcb0b4ee0a4a97d027e0b97b14
jwoz <wozniak.jan@gmail.com>
2013-03-11 14:52:23 +0100

    layout modification

--- src/main.rb
+++ src/main.rb
@@ -1,30 +1,29 @@
-require 'green_shoes'
+@@width = 800
+@@height = 500
 
-
-Shoes.app ({:width=>400, :height=>150, :title=>"FYO2013"}) do
-    @win_w = 400
-    @win_h = 150
+Shoes.app :title=>"FYO2013", :width=>@@width, :height=>@@height do
+    @win_w = @@width
+    @win_h = @@height-27
 
     @top_menu = flow :width=>@win_w do
-        border red
-        @open_button = stack :width=>(@win_w/3) do
-            border blue
-            para "image", :align=>'center'
+        @open_button = stack :width=>(@win_w/3), :height=>25 do
+            background blue
+            para "image", :align=>'center', :border=>'white'
         end
         @open_button.click do
             file = ask_open_file
             main_print_msg(file)
         end
 
-        @filter_button = stack :width=>(@win_w/3) do
-            border green
+        @filter_button = stack :width=>(@win_w/3), :height=>25 do
+            background green
             para "filter", :align=>'center'
         end
         @filter_button.click do
         end
 
-        @exit_button = stack :width=>(@win_w/3) do
-            border black
+        @exit_button = stack :width=>(@win_w/3), :height=>25 do
+            background black
             para "exit", :align=>'center'
         end
         @exit_button.click do
@@ -32,12 +31,56 @@ Shoes.app ({:width=>400, :height=>150, :title=>"FYO2013"}) do
         end
     end
 
-    @main = flow do
-        @t1 = para ""
+    @pictures = flow :width=>@win_w do
+        @src = stack :width=>(@win_w/2-10), :height=>@win_h do
+            background brown
+        end
+
+        @flt = stack :width=>20, :height=>@win_h do
+            background yellow
+        end
+        @flt.click do
+            return if @flt_lock
+            size = @win_w/2 - 10
+            coef = 10
+            diff = (size/2-10)/coef
+                
+            @flt_lock = true
+            if(@flt.width < 30)
+                @a = animate (100) {|i|
+                    @src.width = @src.width - coef 
+                    @flt.width = @flt.width + 2*coef
+                    @dst.width = @dst.width - coef
+                    if i>diff
+                        @a.stop
+                        @flt_lock = false
+                    end
+                }
+            else
+                @a = animate (100) {|i|
+                    @src.width = @src.width + coef
+                    @flt.width = @flt.width - 2*coef
+                    @dst.width = @dst.width + coef
+                    if i>diff
+                        @a.stop
+                        @flt_lock = false
+                    end
+                }
+            end
+        end
+
+        @dst = stack :width=>(@win_w/2-12), :height=>@win_h do
+            background purple
+        end
     end
 
-    def main_print_msg(msg)
-        @t1.text = msg 
-    end
+#    @error_console = flow do
+#        @t1 = para ""
+#    end
+#    @error_console.height = 0
+#
+#    def main_print_msg(msg)
+#        @t1.text = msg 
+#    end
 end
 
